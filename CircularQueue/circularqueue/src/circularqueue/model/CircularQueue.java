4828c26c8d1017a0691fa394736ec89f4360ae50
hoeco <hett.pie@gmail.com>
2022-04-25 12:05:17 -0500

    List of modifications needed included in comments

--- CircularQueue/circularqueue/src/circularqueue/model/CircularQueue.java
+++ CircularQueue/circularqueue/src/circularqueue/model/CircularQueue.java
@@ -28,13 +28,36 @@ public class CircularQueue {
         
     // Constructor of the class:
     public CircularQueue(int n) {
-        innerArray = new int[n];
-        firstPositionPointer = 0;
-        nextPositionPointer = 0;
+        this.innerArray = new int[n];
+        this.firstPositionPointer = 0;
+        this.nextPositionPointer = 0;
     // write the class constructor, it should accept an int parameter "n" as argument and using it to set the length of innerArray
     // initialize the innerArray as a new int array of length "n"
     // initialize the pointers, pointing at the same position (empty array)
     }
+    
+    //
+    //
+    /*  List of Modifications:
+    
+    *** I forgot to include the class attribute "size" it tracks the number of elements stored in the Circular Queue, please create it and change
+        its value in the execution of the methods enqueue(+1) and dequeue(-1).
+    *** In "enqueue" method: 
+            Use null instead of "0", the Circular Queue must be able to manage positive and negative numbers, 0 included
+            Use the expression "nextPositionPointer %= innerArray.length" to return to the index 0.
+    *** In "dequeue" method:
+            is not necessary to check if the position in the array pointed by firstPositionPointer is different to null, just take that value,
+            save it into a new variable, increment firstPositionPointer in 1, apply the modulo operation to it to make it 0 if it is equal to the length of innerArray,
+            and then return the value stored in the new variable created.
+    *** In "isFull" and "isEmpty" methods:
+            The method "isFull" returns True if *The whole queue* is full, false otherwise
+            The method "isEmpty" returns True if *The whole queue* is empty, false otherwise
+            Is not necessary to create the attribute (variable) boolSwitch, instead, use the attribute "size" to know if the whole queue is full or empty,
+            if there are no elements in the queue, then size should be 0, if it is full, then size is equal to the length of the array.
+    */
+    //
+    //
+    
     
     // Methods:
     public void enQueue(int n) {
@@ -52,7 +75,7 @@ public class CircularQueue {
     // Don't forget to move nextPositionPointer to the next position, make use of the modulo operation to return to 0 once the las element of the array has been reached
       
     }
-  
+    
     public int deQueue() {
         if(innerArray[firstPositionPointer] != 0){
            innerArray[firstPositionPointer] = 0;
